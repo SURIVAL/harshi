031fd55c6b9b5935eeda7e5065206e365a64680b
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-02-19 15:57:49 +0530

    Create 109.c

---
+++ 109.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int n,i,mim;
+printf("enter the n number");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+printf("enter the minimum number of value"):
+scanf("%d",&mim);
+}
