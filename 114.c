866e5659e43649fb9bea979b093cefc8a5b4635a
SURIVAL <31478611+SURIVAL@users.noreply.github.com>
2018-02-19 16:06:44 +0530

    Create 114.c

---
+++ 114.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n,k;
+printf("enter the n numbers");
+scanf("%d",&n);
+if(k%2==0)
+{
+printf("enter it is even");
+}
+else
+{
+printf("enter it is odd");
+}
+}
